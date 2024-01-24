e992e0f5cd3ccba7e4c2f471e1c538677f7cef6b
tomifer10 <fernandeztomasig@gmail.com>
2024-01-24 14:38:05 +0100

    Frist Merge
    Sanguineta Merge border bocet

---
+++ src/scripts.ts
@@ -0,0 +1,12 @@
+import { Months } from "./enums";
+
+let date: Date = new Date();
+
+let currentDay: number = date.getDay();
+let currentMonth: number = date.getMonth();
+let currentYear: number = date.getFullYear();
+
+// Function to print the Calendar
+function printCalendar(): void {
+  const firstDay = new Date();
+}
