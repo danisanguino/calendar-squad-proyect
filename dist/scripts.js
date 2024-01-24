3bad2d2b2210711b7fa263234ea5ebcfe26219e0
tomifer10 <fernandeztomasig@gmail.com>
2024-01-24 23:43:12 +0100

    Days succesfully created
    and YES.... IT WORKS!
    Print Day function ready

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,9 +1,24 @@
-"use strict";
+import { domElements } from "./dom.js";
 let date = new Date();
 let currentDay = date.getDay();
 let currentMonth = date.getMonth();
 let currentYear = date.getFullYear();
 function printCalendar() {
-    const firstDay = date;
+    const { currentMonthElement, currentDayElement, daysElement } = domElements;
+    const firstDayOfTheMonth = new Date(currentYear, currentMonth, 1).getDay();
+    const totalDaysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+    daysElement.innerHTML = " ";
+    for (let i = 0; i < firstDayOfTheMonth; i++) {
+        const dayBox = document.createElement("div");
+        dayBox.classList.add("dynamic-day");
+        daysElement.appendChild(dayBox);
+    }
+    for (let i = 1; i <= totalDaysInMonth; i++) {
+        const dayBox = document.createElement("div");
+        dayBox.classList.add("dynamic-day");
+        dayBox.innerText = i.toString();
+        daysElement.appendChild(dayBox);
+    }
+    currentMonthElement.innerText = "";
 }
-console.log(currentMonth);
+printCalendar();
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,5 +1,6 @@
-// import { Months } from "./enums";
-// import { domElements } from "./dom";
+import { Months } from "./enums";
+
+import { domElements } from "./dom.js";
 
 let date: Date = new Date();
 
@@ -8,6 +9,37 @@ let currentMonth: number = date.getMonth();
 let currentYear: number = date.getFullYear();
 
 // Function to print the Calendar
-function printCalendar(): void {}
+function printCalendar(): void {
+  const { currentMonthElement, currentDayElement, daysElement } = domElements;
 
-console.log(currentMonth);
+  const firstDayOfTheMonth: number = new Date(
+    currentYear,
+    currentMonth,
+    1
+  ).getDay();
+  const totalDaysInMonth: number = new Date(
+    currentYear,
+    currentMonth + 1,
+    0
+  ).getDate();
+
+  // clean Calendar content
+  daysElement.innerHTML = " ";
+
+  // Add empty days element before first day
+  for (let i = 0; i < firstDayOfTheMonth; i++) {
+    const dayBox = document.createElement("div");
+    dayBox.classList.add("dynamic-day");
+    daysElement.appendChild(dayBox);
+  }
+  // Printing Days of actual month
+  for (let i = 1; i <= totalDaysInMonth; i++) {
+    const dayBox = document.createElement("div");
+    dayBox.classList.add("dynamic-day");
+    dayBox.innerText = i.toString();
+    daysElement.appendChild(dayBox);
+  }
+  currentMonthElement.innerText = "";
+}
+
+printCalendar();
